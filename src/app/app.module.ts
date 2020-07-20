6bc0d9ddb7b0123d39f8797b1295f42afba3c2ff
atulzambre <atul.zambre123@gmail.com>
2020-07-20 09:19:11 +0530

    first angular 10 project

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginPageComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
